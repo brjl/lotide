07acef1904521a227455585331325c59122236f3
Liam Butler <lrbutler@gmail.com>
2021-02-02 00:17:37 +0000

    tail function

---
+++ tail.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  const pass = String.fromCodePoint(0x1f60d);
+  const fail = String.fromCodePoint(0x1f631);
+
+  if (actual === expected) {
+    console.log(
+      `${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`
+    );
+  } else {
+    console.log(
+      `${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+const tail = function(array1) {
+  return array1.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const empty = [];
+tail(empty);
+assertEqual(empty.length, empty);
