4f5419a99149a9251ca97d1b93ebb2751f90f175
Liam Butler <lrbutler@gmail.com>
2021-02-03 20:20:12 +0000

    Added countOnly and wip flatten

---
+++ countOnly.js
@@ -0,0 +1,63 @@
+const assertEqual = function(actual, expected) {
+  const pass = String.fromCodePoint(0x1f60d);
+  const fail = String.fromCodePoint(0x1f631);
+
+  if (actual === expected) {
+    console.log(
+      `${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`
+    );
+  } else {
+    console.log(
+      `${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    console.log(item);
+
+    // inside the loop,
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
+
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
