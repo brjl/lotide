c07142dc8c9f9afce9fb722a6a3559d9e22dd721
Liam Butler <lrbutler@gmail.com>
2021-02-03 02:00:00 +0000

    eq Array function added

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  const pass = String.fromCodePoint(0x1f60d);
+  const fail = String.fromCodePoint(0x1f631);
+
+  if (actual === expected) {
+    console.log(
+      `${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`
+    );
+  } else {
+    console.log(
+      `${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
