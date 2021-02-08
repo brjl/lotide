405921fbcd5f029bca8c6fa35bfadd4981587c20
Liam Butler <lrbutler@gmail.com>
2021-02-08 22:13:48 +0000

    Middle fixed and refactored mocha test

--- middle.js
+++ middle.js
@@ -1,16 +1,15 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
-
-
-const middle = function(array) {
+const middle = function (array) {
   const mid = array.length / 2;
-  if (array.length > 2 && array.length % 2 === 0) {
-    return [array[mid - 1], array[mid]];
-  } else if (array.length <= 2) {
+  if (array.length <= 2) {
     return [];
+  } else if (array.length > 2 && array.length % 2 === 0) {
+    return [array[mid - 1], array[mid]];
+  } else {
+    return [array[Math.floor(mid)]];
   }
-  return [array[Math.floor(mid)]];
 };
 
 module.exports = middle;
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,34 +1,32 @@
-const middle = require('../middle');
 const assert = require("chai").assert;
+const middle = require("../middle");
 
 describe("#middle", () => {
   it("returns 3 for [1, 2, 3, 4, 5]", () => {
-    assert.strictEqual(middle([1, 2, 3, 4, 5]), 3);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
 
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
-    assert.strictEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
 
   it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
-    assert.strictEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
 
   it("returns 5 for [5, 6, 7]", () => {
-    assert.strictEqual(middle([5, 6, 7]), 6);
+    assert.deepEqual(middle([5, 6, 7]), [6]);
   });
 
   it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(middle(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
+    assert.deepEqual(middle(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"]);
   });
-  
+
   it("returns [] for [1, 2]", () => {
-    assert.strictEqual(middle([1, 2]), []);
+    assert.deepEqual(middle([1, 2]), []);
   });
 
   it("returns [] for [1]", () => {
-    assert.strictEqual(middle([1]), []);
+    assert.deepEqual(middle([1]), []);
   });
-  
 });
-
